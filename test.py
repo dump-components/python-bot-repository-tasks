b23f5f601c18f2887ead82a73390700fef4f8338
Samuel Sublate <samuelsublate.dev@gmail.com>
2022-08-19 12:22:19 -0300

    del files

--- test.py
+++
@@ -1,5 +0,0 @@
-from base_model.model_exemple import ModelExemple
-from repository.RepositoryFromTask import RepositoryFromTask
-
-repository = RepositoryFromTask()
-RepositoryFromTask.create_table(repository.database, ModelExemple)
